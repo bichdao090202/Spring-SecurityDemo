9c27ebe5906fa9866106ce31a8f950df6541b0a8
bichdao090202 <bichdao090202@gmail.com>
2023-12-11 20:23:01 +0700

    update

--- src/main/java/vn/edu/iuh/fit/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/vn/edu/iuh/fit/springsecuritydemo/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,48 +18,29 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class SecurityConfig {
     @Autowired
-    public void globalConfig(AuthenticationManagerBuilder auth, PasswordEncoder encoder, DataSource dataSource )throws Exception{
+    public void configureGlobal(AuthenticationManagerBuilder auth, PasswordEncoder encoder, DataSource dataSource) throws Exception{
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .withDefaultSchema()
+                .withUser(User.withUsername("user")
+                        .password(encoder.encode("user"))
+                        .roles("USER"))
                 .withUser(User.withUsername("admin")
                         .password(encoder.encode("admin"))
-                        .roles("ADMIN")
-                        .build())
-                .withUser(User.withUsername("teo")
-                        .password(encoder.encode("teo"))
-                        .roles("TEO")
-                        .build())
-                .withUser(User.withUsername("ty")
-                        .password(encoder.encode("ty"))
-                        .roles("USER")
-                        .build())
-        ;
+                        .roles("ADMIN","USER")
+                );
     }
-
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth, PasswordEncoder encoder, DataSource dataSource) throws Exception{
-//        auth.jdbcAuthentication()
-//                .dataSource(dataSource)
-//                .withDefaultSchema()
-//                .withUser(User.withUsername("user")
-//                        .password(encoder.encode("user"))
-//                        .roles("USER"))
-//                .withUser(
-//                        User.withUsername("admin")
-//                                .password(encoder.encode("admin"))
-//                                .roles("ADMIN","USER")
-//                );
-//    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth->auth
-                        .requestMatchers("/","/home","/index").permitAll()
-                        .requestMatchers("/api/**").hasAnyRole("ADMIN","USER","TEO")
-                        .requestMatchers(("/admin/**")).hasRole("ADMIN")
-                        .anyRequest().permitAll()
+                .requestMatchers("/","/home","/index").permitAll()
+                .requestMatchers("/api/**").hasAnyRole("ADMIN","USER","NV")
+                .requestMatchers(("/admin/**")).hasRole("ADMIN")
+                .requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().permitAll()
         );
+        http.csrf(csrf->csrf.ignoringRequestMatchers("/h2-console/**"));
+        http.headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
--- src/main/java/vn/edu/iuh/fit/springsecuritydemo/controllers/SampleController.java
+++ src/main/java/vn/edu/iuh/fit/springsecuritydemo/controllers/SampleController.java
@@ -1,12 +1,11 @@
 package vn.edu.iuh.fit.springsecuritydemo.controllers;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class EmployeeController {
+public class SampleController {
     @GetMapping
     public String hello(){
         return "hello world";
